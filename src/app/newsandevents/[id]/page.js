4688860a21a0b5eb2f7c2c19ef1832bcb30a12dd
Shamim Sheikh <shamimsheikh@Shamims-MacBook.local>
2024-07-03 14:45:58 +0530

    updated

---
+++ src/app/newsandevents/[id]/page.js
@@ -0,0 +1,58 @@
+'use client';
+
+import { useParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import Head from 'next/head';
+import FooterSection from '@/components/footer/FooterSection';
+import Navbar from '@/components/navbar/Navbar';
+
+export default function PostPage() {
+    const { id } = useParams();
+    const [post, setPost] = useState(null);
+
+    useEffect(() => {
+        if (id) {
+            fetch(`https://chanrericr.com/blog/api/get_single_post.php?id=${id}`)
+                .then(response => response.json())
+                .then(data => {
+                    setPost(data);
+                })
+                .catch(error => console.error('Error:', error));
+        }
+    }, [id]);
+
+    if (!post) return <div>
+        <Navbar/>
+        
+        <div className="flex justify-center items-center min-h-screen">
+      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
+    </div>
+
+        <FooterSection />
+        
+        </div>;
+
+    // Parse the PostingDate string into a Date object and format it
+    const formattedDate = new Date(post.PostingDate).toLocaleDateString();
+
+    return (
+        <>
+        <Navbar/>
+            <Head>
+                <title>{post.PostTitle}</title>
+                <meta name="description" content={post.PostTitle} />
+            </Head>
+            <div className="container mx-auto px-4 lg:px-24 py-8">
+                <h1 className="text-3xl text-center font-bold mb-4">{post.PostTitle}</h1>
+                <img
+                    src={`https://chanrericr.com/blog/admin/postimages/${post.PostImage}`}
+                    alt={post.PostTitle}
+                    className="w-[50%] mx-auto h-auto mb-4"
+                />
+                <div dangerouslySetInnerHTML={{ __html: post.PostDetails }} />
+                <p className="mt-4 text-sm text-base-content opacity-50">Posted on: {formattedDate}</p>
+            </div>
+            <FooterSection />
+        </>
+    );
+}
--- src/components/blog/BlogPage.js
+++ src/components/blog/BlogPage.js
@@ -4,18 +4,22 @@ import Head from 'next/head';
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import FooterSection from '../footer/FooterSection';
+import Link from 'next/link';
 
 export default function BlogPage() {
+  const [isLoading, setIsLoading] = useState(true);
   const [currentPage, setCurrentPage] = useState(1);
   const postsPerPage = 6;
 
   const [blogposts, setBlogPosts] = useState([]);
 console.log(blogposts)
-  const fetchData = () =>{
-    fetch('https://chanrericr.com/blog/api/posts.php')
+  const fetchData =async () =>{
+    setIsLoading(true);
+   await fetch('https://chanrericr.com/blog/api/posts.php')
      .then(response => response.json())
      .then(data => {
         setBlogPosts(data);
+        setIsLoading(false);
       })
      .catch(error => console.error('Error:', error));
   }
@@ -24,79 +28,14 @@ console.log(blogposts)
 fetchData();
   },[])
 
-  // const posts = [
-  //   {
-  //     id: 1,
-  //     title: "Getting Started with Next.js",
-  //     excerpt: "Learn how to build modern web applications with Next.js and React.",
-  //     coverImage: "https://picsum.photos/seed/post1/600/400",
-  //     date: "May 1, 2024"
-  //   },
-  //   {
-  //     id: 2,
-  //     title: "Styling with DaisyUI",
-  //     excerpt: "Discover how to create beautiful UIs quickly using DaisyUI and Tailwind CSS.",
-  //     coverImage: "https://picsum.photos/seed/post2/600/400",
-  //     date: "May 5, 2024"
-  //   },
-  //   {
-  //     id: 3,
-  //     title: "Advanced React Patterns",
-  //     excerpt: "Explore advanced React patterns to write cleaner, more efficient code.",
-  //     coverImage: "https://picsum.photos/seed/post3/600/400",
-  //     date: "May 10, 2024"
-  //   },
-  //   {
-  //     id: 4,
-  //     title: "Mastering CSS Grid",
-  //     excerpt: "Deep dive into CSS Grid and create complex layouts with ease.",
-  //     coverImage: "https://picsum.photos/seed/post4/600/400",
-  //     date: "May 15, 2024"
-  //   },
-  //   {
-  //     id: 5,
-  //     title: "JavaScript ES6+ Features",
-  //     excerpt: "Explore the powerful features introduced in recent JavaScript versions.",
-  //     coverImage: "https://picsum.photos/seed/post5/600/400",
-  //     date: "May 20, 2024"
-  //   },
-  //   {
-  //     id: 6,
-  //     title: "Responsive Design Principles",
-  //     excerpt: "Learn key principles for creating websites that look great on any device.",
-  //     coverImage: "https://picsum.photos/seed/post6/600/400",
-  //     date: "May 25, 2024"
-  //   },
-  //   {
-  //     id: 7,
-  //     title: "Intro to GraphQL",
-  //     excerpt: "Understand the basics of GraphQL and how it differs from REST APIs.",
-  //     coverImage: "https://picsum.photos/seed/post7/600/400",
-  //     date: "June 1, 2024"
-  //   },
-  //   {
-  //     id: 8,
-  //     title: "Web Accessibility Guidelines",
-  //     excerpt: "Ensure your websites are accessible to all users with these key guidelines.",
-  //     coverImage: "https://picsum.photos/seed/post8/600/400",
-  //     date: "June 5, 2024"
-  //   },
-  //   {
-  //     id: 9,
-  //     title: "State Management in React",
-  //     excerpt: "Compare different state management solutions for React applications.",
-  //     coverImage: "https://picsum.photos/seed/post9/600/400",
-  //     date: "June 10, 2024"
-  //   },
-  //   {
-  //     id: 10,
-  //     title: "Progressive Web Apps",
-  //     excerpt: "Learn how to transform your website into a Progressive Web App.",
-  //     coverImage: "https://picsum.photos/seed/post10/600/400",
-  //     date: "June 15, 2024"
-  //   }
-  // ];
 
+  const Loader = () => (
+    <div className="flex justify-center items-center h-64">
+      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
+    </div>
+  );
+
+ 
   const indexOfLastPost = currentPage * postsPerPage;
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
   const currentPosts = blogposts.slice(indexOfFirstPost, indexOfLastPost);
@@ -116,9 +55,11 @@ fetchData();
           <h1 className="text-2xl font-bold  mb-2">News & Events</h1>
           <p className="text-md opacity-70">Explore the latest Blog, NEWS and Events</p>
         </header>
-
+        {isLoading ? (
+        <Loader />
+      ) : (
         <main className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {currentPosts.map((post) => (
+          {currentPosts?.map((post) => (
             <article key={post.id} className="card bg-base-100 shadow-xl hover:shadow-2xl transition-shadow duration-300">
               <figure>
                 <img
@@ -131,15 +72,17 @@ fetchData();
               </figure>
               <div className="card-body">
                 <h2 className="card-title text-lg  mb-2">{post.PostTitle}</h2>
-                <p className="text-base-content opacity-70 mb-4">{post.excerpt}</p>
                 <div className="card-actions justify-between items-center">
-                  <span className="text-sm text-base-content opacity-50">{post.date}</span>
-                  <button className="btn bg-activeColor text-white btn-sm">Read More</button>
+                  <span className="text-sm text-base-content opacity-50">{post.PostingDate}</span>
+                  <Link href={`/newsandevents/${post.id}`}>
+            <button className="btn bg-activeColor text-white btn-sm">Read More</button>
+          </Link>
                 </div>
               </div>
             </article>
           ))}
         </main>
+      )};
 
         <div className="flex justify-center mt-8">
           <div className="btn-group">
